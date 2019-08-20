a7a4e317b10253ef7c16a33daa7f312d22fae918
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2019-08-20 09:45:30 +0200

    fix: cmd windows launch script actually works

--- core/exe/gaiasky.cmd
+++ core/exe/gaiasky.cmd
@@ -9,30 +9,32 @@ SETLOCAL ENABLEEXTENSIONS
 :: Gaia Sky location
 SET GSDIR=%~dp0
 
+cd %GSDIR%
+
 :: Memory
 SET OPTS=-Xms2g -Xmx4g
 :: Garbage Collector G1
 SET OPTS=%OPTS% -XX:+UseG1GC
 :: Assets location
-SET OPTS=%OPTS% -Dassets.location=%GSDIR%
+SET OPTS=%OPTS% -Dassets.location=.
 :: SimpleLogger defaults
 SET OPTS=%OPTS% -Dorg.slf4j.simpleLogger.defaultLogLevel=warn -Dorg.slf4j.simpleLogger.showThreadName=false
 :: Non-ASCII characters workaround for Jython (see #281)
 SET OPTS=%OPTS% -Dpython.console.encoding=UTF-8
 
-IF EXIST %GSDIR%jre\bin\java.exe (
+IF EXIST ".\jre\bin\java.exe" (
     :: Use bundled java
-    SET JAVA_CMD=%GSDIR%jre\bin\java.exe
+    SET JAVA_CMD=.\jre\bin\java.exe
 ) ELSE (
     :: Look for java
     IF "%JAVA_HOME%"=="" (
         ECHO Java installation not found! Exiting
         GOTO :END
     ) ELSE (
-        SET JAVA_CMD=%JAVA_HOME%\bin\java.exe
+        SET JAVA_CMD="%JAVA_HOME%\bin\java.exe"
     )
 )
 @ECHO ON
 :: Run
-"%JAVA_CMD%" %OPTS% -cp "%GSDIR%lib\*" gaia.cu9.ari.gaiaorbit.desktop.GaiaSkyDesktop
-:END
+%JAVA_CMD% %OPTS% -cp .\lib\* gaia.cu9.ari.gaiaorbit.desktop.GaiaSkyDesktop
+::END
