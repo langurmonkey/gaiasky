5837140d57bd421b3cb59801d60618c54cd44d56
langurmonkey <me@tonisagrista.com>
2019-08-21 10:38:38 +0200

    fix: controller mappings format error in loading

--- core/src/gaia/cu9/ari/gaiaorbit/interfce/ControllerMappings.java
+++ core/src/gaia/cu9/ari/gaiaorbit/interfce/ControllerMappings.java
@@ -27,23 +27,32 @@ public class ControllerMappings extends AbstractControllerMappings {
             mappings.load(is);
             is.close();
 
-            AXIS_ROLL = Integer.parseInt(mappings.getProperty("axis.roll", "-1"));
-            AXIS_PITCH = Integer.parseInt(mappings.getProperty("axis.pitch", "-1"));
-            AXIS_YAW = Integer.parseInt(mappings.getProperty("axis.yaw", "-1"));
-            AXIS_MOVE = Integer.parseInt(mappings.getProperty("axis.move", "-1"));
-            AXIS_VEL_UP = Integer.parseInt(mappings.getProperty("axis.velocityup", "-1"));
-            AXIS_VEL_DOWN = Integer.parseInt(mappings.getProperty("axis.velocitydown", "-1"));
+            AXIS_ROLL = parse(mappings, "axis.roll", "-1");
+            AXIS_PITCH = parse(mappings,"axis.pitch", "-1");
+            AXIS_YAW = parse(mappings, "axis.yaw", "-1");
+            AXIS_MOVE = parse(mappings,"axis.move", "-1");
+            AXIS_VEL_UP = parse(mappings, "axis.velocityup", "-1");
+            AXIS_VEL_DOWN = parse(mappings,"axis.velocitydown", "-1");
 
-            BUTTON_VEL_UP = Integer.parseInt(mappings.getProperty("button.velocityup", "-1"));
-            BUTTON_VEL_DOWN = Integer.parseInt(mappings.getProperty("button.velocitydown", "-1"));
-            BUTTON_VEL_MULT_TENTH = Integer.parseInt(mappings.getProperty("button.velocitytenth", "-1"));
-            BUTTON_VEL_MULT_HALF = Integer.parseInt(mappings.getProperty("button.velocityhalf", "-1"));
-            BUTTON_UP = Integer.parseInt(mappings.getProperty("button.up", "-1"));
-            BUTTON_DOWN = Integer.parseInt(mappings.getProperty("button.down", "-1"));
-            BUTTON_MODE_TOGGLE = Integer.parseInt(mappings.getProperty("button.mode.toggle", "-1"));
+            BUTTON_VEL_UP = parse(mappings,"button.velocityup", "-1");
+            BUTTON_VEL_DOWN = parse(mappings,"button.velocitydown", "-1");
+            BUTTON_VEL_MULT_TENTH = parse(mappings,"button.velocitytenth", "-1");
+            BUTTON_VEL_MULT_HALF = parse(mappings,"button.velocityhalf", "-1");
+            BUTTON_UP = parse(mappings,"button.up", "-1");
+            BUTTON_DOWN = parse(mappings,"button.down", "-1");
+            BUTTON_MODE_TOGGLE = parse(mappings,"button.mode.toggle", "-1");
 
         } catch (Exception e) {
             Logger.getLogger(this.getClass()).error(e, "Error reading inputListener mappings");
+        }
+    }
+
+
+    private int parse(Properties mappings, String property, String defaultValue){
+        try{
+            return Integer.parseInt(mappings.getProperty(property, defaultValue));
+        } catch (Exception e){
+            return Integer.parseInt(defaultValue);
         }
     }
 
