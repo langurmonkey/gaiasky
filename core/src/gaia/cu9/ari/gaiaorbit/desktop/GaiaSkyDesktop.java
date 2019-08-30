050da8e429e86a85d70540489902f0d497c48f3f
langurmonkey <me@tonisagrista.com>
2019-08-30 14:14:31 +0200

    build: add VR launcher

--- core/src/gaia/cu9/ari/gaiaorbit/desktop/GaiaSkyDesktop.java
+++ core/src/gaia/cu9/ari/gaiaorbit/desktop/GaiaSkyDesktop.java
@@ -66,7 +66,7 @@ public class GaiaSkyDesktop implements IObserver {
      *
      * Leading zeroes are omitted to avoid octal literal interpretation.
      */
-    public static int SOURCE_CONF_VERSION = 20200;
+    public static int SOURCE_CONF_VERSION = 20201;
     private static GaiaSkyDesktop gsd;
     private static boolean REST_ENABLED = false;
     private static Class<?> REST_SERVER_CLASS = null;
