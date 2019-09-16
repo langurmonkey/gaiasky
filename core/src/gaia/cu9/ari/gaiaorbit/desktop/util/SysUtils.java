5f3a3c37a5e6af12e62fb30afbb84b99ad78fd6b
langurmonkey <me@tonisagrista.com>
2019-09-16 14:07:58 +0200

    fix: java version string without minor or revision
    Fix java version string with only major version (like early builds).
    Fixes #302.

--- core/src/gaia/cu9/ari/gaiaorbit/desktop/util/SysUtils.java
+++ core/src/gaia/cu9/ari/gaiaorbit/desktop/util/SysUtils.java
@@ -256,9 +256,13 @@ public class SysUtils {
 
     public static double getJavaVersion() {
         String version = System.getProperty("java.version");
-        int pos = version.indexOf('.');
-        pos = version.indexOf('.', pos + 1);
-        return Double.parseDouble(version.substring(0, pos)); //-V6009
+        if(version.contains(("."))) {
+            int pos = version.indexOf('.');
+            pos = version.indexOf('.', pos + 1);
+            return Double.parseDouble(version.substring(0, pos));
+        }else{
+            return Double.parseDouble(version);
+        }
     }
 
 }
