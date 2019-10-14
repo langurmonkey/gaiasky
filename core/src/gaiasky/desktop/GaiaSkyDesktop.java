a71212676b01e80e96500e6bcb16f0f078040bb1
langurmonkey <me@tonisagrista.com>
2019-10-14 14:25:22 +0200

    build: update source version number to 020202

--- core/src/gaiasky/desktop/GaiaSkyDesktop.java
+++ core/src/gaiasky/desktop/GaiaSkyDesktop.java
@@ -68,7 +68,7 @@ public class GaiaSkyDesktop implements IObserver {
      *
      * Leading zeroes are omitted to avoid octal literal interpretation.
      */
-    public static int SOURCE_CONF_VERSION = 20201;
+    public static int SOURCE_CONF_VERSION = 20202;
     private static GaiaSkyDesktop gsd;
     private static boolean REST_ENABLED = false;
     private static Class<?> REST_SERVER_CLASS = null;
