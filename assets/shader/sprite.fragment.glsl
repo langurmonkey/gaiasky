8dd58e5e750828263e59e47eec6fc61015065239
langurmonkey <me@tonisagrista.com>
2019-10-14 13:33:50 +0200

    none: velocity map in sprites

--- assets/shader/sprite.fragment.glsl
+++ assets/shader/sprite.fragment.glsl
@@ -11,6 +11,7 @@ in vec2 v_texCoords;
 in vec4 v_color;
 
 layout (location = 0) out vec4 fragColor;
+layout (location = 1) out vec4 velMap;
 
 vec4 draw() {
     vec4 tex = texture(u_texture0, v_texCoords);
@@ -22,4 +23,5 @@ void main() {
 
     // Logarithmic depth buffer
     gl_FragDepth = getDepthValue(u_zfar, u_k);
+    velMap = vec4(0.0);
 }
