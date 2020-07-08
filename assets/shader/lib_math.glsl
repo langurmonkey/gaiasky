b19516f2e89e88529d542ceb7205efe3b872b86f
langurmonkey <me@tonisagrista.com>
2020-07-08 09:05:56 +0200

    fix: shader lint function

--- assets/shader/lib_math.glsl
+++ assets/shader/lib_math.glsl
@@ -5,7 +5,7 @@ float lint2(float x, float x0, float x1, float y0, float y1) {
 }
 
 float lint(float x, float x0, float x1, float y0, float y1) {
-    if(x <= x0) x = x0;
-    if(x >= x1) x = x1;
+    if(x <= x0) return y0;
+    if(x >= x1) return y1;
     return y0 + (y1 - y0) * smoothstep(x0, x1, x);
 }
