777d638a3464b13a9cf827cbdff11818749c7f46
langurmonkey <me@tonisagrista.com>
2020-11-04 16:17:47 +0100

    none: disable blur in recursive grid shader

--- assets/shader/default.gridrec.fragment.glsl
+++ assets/shader/default.gridrec.fragment.glsl
@@ -29,10 +29,6 @@ in vec4 v_color;
 layout (location = 0) out vec4 fragColor;
 layout (location = 1) out vec4 velMap;
 
-#ifdef velocityBufferFlag
-#include shader/lib_velbuffer.frag.glsl
-#endif
-
 #define PI 3.141592
 #define N 10.0
 #define WIDTH 2.0
@@ -98,8 +94,5 @@ void main() {
     fragColor = circle(v_texCoords0);
 
     gl_FragDepth = getDepthValue(u_cameraNearFar.y, u_cameraK);
-
-    #ifdef velocityBufferFlag
-    velocityBuffer();
-    #endif
+    velMap = vec4(0.0, 0.0, 0.0, 1.0);
 }
