langurmonkey/gaiasky

commit 40b99a2bdfe181a305b9bb23664b33c92d5af507

feat: add white core to star shaders

assets/shader/star.group.fragment.glsl
@@ -34,7 +34,10 @@ void main() {
         discard;
     }
 
-    fragColor = alpha * v_col;
+    // White core
+    float core = 1.0 - smoothstep(0.0, 0.04, distance(vec2(0.5), uv) * 2.0);
+    // Final color
+    fragColor = clamp(alpha * v_col + core * 2.0, 0.0, 1.0);
     gl_FragDepth = getDepthValue(u_zfar, u_k);
 
     #ifdef velocityBufferFlag

commit 0f279d8af8a5f372c881c570cd1814a98c73565a

fix: bug introduced in 40b99a2 - star cores not applied alpha - fixes #352

assets/shader/star.group.fragment.glsl
@@ -37,7 +37,7 @@ void main() {
     // White core
     float core = 1.0 - smoothstep(0.0, 0.04, distance(vec2(0.5), uv) * 2.0);
     // Final color
-    fragColor = clamp(alpha * v_col + core * 2.0, 0.0, 1.0);
+    fragColor = clamp(alpha * (v_col + core * 2.0), 0.0, 1.0);
     gl_FragDepth = getDepthValue(u_zfar, u_k);
 
     #ifdef velocityBufferFlag