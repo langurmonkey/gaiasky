10217f2ca2c7cece8082ff00e47753fccc696d79
langurmonkey <me@tonisagrista.com>
2021-10-22 12:40:39 +0200

    none: add point cloud setting to be able to change between render systems

--- core/src/gaiasky/util/Settings.java
+++ core/src/gaiasky/util/Settings.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import gaiasky.GaiaSky;
 import gaiasky.desktop.util.camera.CameraKeyframeManager;
 import gaiasky.event.EventManager;
