ea2f84b39647b497fbaa0ab14f3cb301b6c4562c
langurmonkey <me@tonisagrista.com>
2021-12-01 15:47:31 +0100

    fix: reflected cubemap orientation (was upside down)

--- assets/shader/normal.fragment.glsl
+++ assets/shader/normal.fragment.glsl
@@ -372,7 +372,7 @@ void main() {
             // Perturb the normal to get reflect direction
             pullNormal();
             mat3 TBN = cotangentFrame(g_normal, -v_data.viewDir, texCoords);
-			vec3 reflectDir = normalize(reflect(-v_data.fragPosWorld, normalize(TBN * N)));
+			vec3 reflectDir = normalize(reflect(v_data.fragPosWorld, normalize(TBN * N)));
 		#endif // environmentCubemapFlag
     #else
 	    // Normal in tangent space
