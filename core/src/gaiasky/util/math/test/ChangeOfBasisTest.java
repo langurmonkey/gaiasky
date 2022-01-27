d5f1cf232d3a45ccd432ff93a7881ded764ed207
langurmonkey <me@tonisagrista.com>
2022-01-27 20:38:46 +0100

    none: some fixes

--- core/src/gaiasky/util/math/test/ChangeOfBasisTest.java
+++ core/src/gaiasky/util/math/test/ChangeOfBasisTest.java
@@ -8,6 +8,7 @@ import gaiasky.util.math.Vector3d;
  */
 public class ChangeOfBasisTest {
     public static void main(String[] args) {
+        System.out.println("==========================");
         Matrix4d c = Matrix4d.changeOfBasis(new double[] { 0, 0, -1 }, new double[] { 0, 1, 0 }, new double[] { 1, 0, 0 });
 
         Vector3d v = new Vector3d(1, 0, 0);
@@ -17,6 +18,16 @@ public class ChangeOfBasisTest {
         System.out.println(v + " -> " + (new Vector3d(v)).mul(c));
 
         v = new Vector3d(0, 0, 1);
+        System.out.println(v + " -> " + (new Vector3d(v)).mul(c));
+
+
+        System.out.println("==========================");
+        c = Matrix4d.changeOfBasis(new double[] { 0.5, 0, 0 }, new double[] { -1, 2, 0 }, new double[] { 0, 0, 1 });
+
+        v = new Vector3d(4, 1, 0);
+        System.out.println(v + " -> " + (new Vector3d(v)).mul(c));
+
+        v = new Vector3d(0, 1, 0);
         System.out.println(v + " -> " + (new Vector3d(v)).mul(c));
     }
 }
