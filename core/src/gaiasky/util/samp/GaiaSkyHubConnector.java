25fb233862c97c1d68d94817d3f196ef7d00f819
langurmonkey <me@tonisagrista.com>
2022-02-05 10:21:34 +0100

    none: fix regression in hub connector

--- core/src/gaiasky/util/samp/GaiaSkyHubConnector.java
+++ core/src/gaiasky/util/samp/GaiaSkyHubConnector.java
@@ -32,7 +32,7 @@ public class GaiaSkyHubConnector extends HubConnector {
         try {
             hubName = super.getConnection().getRegInfo().getHubId();
         }catch (NullPointerException | SampException ignored) {}
-        
+
         logger.info(isConnected ? I18n.txt("samp.connected", hubName) : I18n.txt("samp.disconnected"));
         EventManager.instance.post(Events.POST_POPUP_NOTIFICATION, isConnected ? I18n.txt("samp.connected", hubName) : I18n.txt("samp.disconnected"));
     }
