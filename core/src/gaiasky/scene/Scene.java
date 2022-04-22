31630d8bbb5533fd17b590acb43ff91c87caed42
langurmonkey <me@tonisagrista.com>
2022-04-23 00:42:38 +0200

    none: constellations to JSON format. New loader loads constellations and more.

--- core/src/gaiasky/scene/Scene.java
+++ core/src/gaiasky/scene/Scene.java
@@ -350,6 +350,10 @@ public class Scene {
             addArchetype(ConstellationBoundaries.class.getName(), SceneGraphNode.class.getName(),
                     Boundaries.class);
 
+            // Constellation
+            addArchetype(Constellation.class.getName(), SceneGraphNode.class.getName(),
+                    Constel.class);
+
         } else {
             logger.error("World is null, can't initialize archetypes.");
         }
