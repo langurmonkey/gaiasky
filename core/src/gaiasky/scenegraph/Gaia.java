4b5b4dcd3e83b9991b27d69e186f5517cde3fd56
langurmonkey <me@tonisagrista.com>
2022-04-26 10:36:46 +0200

    refactor: abstract attitude loading system, remove gaia class, use heliotropic satellite.

---
+++ core/src/gaiasky/scenegraph/Gaia.java
@@ -0,0 +1,25 @@
+/*
+ * This file is part of Gaia Sky, which is released under the Mozilla Public License 2.0.
+ * See the file LICENSE.md in the project root for full license details.
+ */
+
+package gaiasky.scenegraph;
+
+/**
+ * The Gaia satellite is just a heliotropic satellite. It is now phased out.
+ * @deprecated Use {@link HeliotropicSatellite} instead.
+ * TODO Remove this when possible.
+ */
+public class Gaia extends HeliotropicSatellite {
+
+    public Gaia() {
+        super();
+    }
+
+    public void initialize() {
+        provider = "gaiasky.util.gaia.GaiaAttitudeServer";
+        attitudeLocation = "data/attitudexml";
+        super.initialize();
+    }
+
+}
