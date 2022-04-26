399b6a5abdd0cbb6a589df5ff15587bd4f2ffcc3
langurmonkey <me@tonisagrista.com>
2022-04-26 15:31:14 +0200

    none: remove special back-buffer config with 24-bit z-buffer.

--- core/src/gaiasky/desktop/GaiaSkyDesktop.java
+++ core/src/gaiasky/desktop/GaiaSkyDesktop.java
@@ -319,8 +319,6 @@ public class GaiaSkyDesktop implements IObserver {
         if (cliArgs.debugGpu) {
             cfg.enableGLDebugOutput(true, System.out);
         }
-        // Color, Depth, stencil buffers, MSAA
-        cfg.setBackBufferConfig(8, 8, 8, 8, 24, 8, 0);
 
         // Launch app
         try {
