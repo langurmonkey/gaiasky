030ffc9ad0525ab09bc67c17c0a0463ef2762eb0
langurmonkey <me@tonisagrista.com>
2022-04-27 09:57:55 +0200

    none: revert back-buffer config removal, did not fix issue 394.

--- core/src/gaiasky/desktop/GaiaSkyDesktop.java
+++ core/src/gaiasky/desktop/GaiaSkyDesktop.java
@@ -319,6 +319,8 @@ public class GaiaSkyDesktop implements IObserver {
         if (cliArgs.debugGpu) {
             cfg.enableGLDebugOutput(true, System.out);
         }
+        // Color, Depth, stencil buffers, MSAA
+        cfg.setBackBufferConfig(8, 8, 8, 8, 24, 8, 0);
 
         // Launch app
         try {
