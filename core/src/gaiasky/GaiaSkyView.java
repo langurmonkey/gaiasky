d585f1bfe07b3a7f481b15d9300fb037fec4ffd4
langurmonkey <me@tonisagrista.com>
2022-08-27 21:44:30 +0200

    none: fix comment.

--- core/src/gaiasky/GaiaSkyView.java
+++ core/src/gaiasky/GaiaSkyView.java
@@ -3,10 +3,7 @@
  * See the file LICENSE.md in the project root for full license details.
  */
 
-package gaiasky;/*
- * This file is part of Gaia Sky, which is released under the Mozilla Public License 2.0.
- * See the file LICENSE.md in the project root for full license details.
- */
+package gaiasky;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
