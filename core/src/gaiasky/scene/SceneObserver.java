09c441683d3a6667b374cacfefa9b43d13850979
langurmonkey <me@tonisagrista.com>
2022-09-23 15:11:45 +0200

    none: fix individual visibility and force label operations.

--- core/src/gaiasky/scene/SceneObserver.java
+++ core/src/gaiasky/scene/SceneObserver.java
@@ -30,28 +30,23 @@ public class SceneObserver implements IObserver {
     public void notify(Event event, Object source, Object... data) {
         switch (event) {
         case PER_OBJECT_VISIBILITY_CMD -> {
-            if (data[0] instanceof Entity) {
-                var entity = (Entity) data[0];
-                String name = (String) data[1];
-                boolean state = (boolean) data[2];
+            if (data[0] instanceof FocusView) {
+                final var entity = (FocusView) data[0];
+                final String name = (String) data[1];
+                final boolean state = (boolean) data[2];
 
-                synchronized (view) {
-                    view.setEntity(entity);
-                    view.setVisible(state, name.toLowerCase(Locale.ROOT));
-                }
+                entity.setVisible(state, name.toLowerCase());
+                logger.info(I18n.msg("notif.visibility.object.set", entity.getName(), I18n.msg("gui." + state)));
             }
         }
         case FORCE_OBJECT_LABEL_CMD -> {
-            if (data[0] instanceof Entity) {
-                final Entity entity = (Entity) data[0];
-                String name = (String) data[1];
-                boolean state = (boolean) data[2];
+            if (data[0] instanceof FocusView) {
+                final var entity = (FocusView) data[0];
+                final String name = (String) data[1];
+                final boolean state = (boolean) data[2];
 
-                synchronized (view) {
-                    view.setEntity(entity);
-                    view.setForceLabel(state, name.toLowerCase(Locale.ROOT));
-                    logger.info(I18n.msg("notif.object.flag", "forceLabel", view.getName(), I18n.msg("gui." + state)));
-                }
+                entity.setForceLabel(state, name.toLowerCase(Locale.ROOT));
+                logger.info(I18n.msg("notif.object.flag", "forceLabel", entity.getName(), I18n.msg("gui." + state)));
             }
         }
         case LABEL_COLOR_CMD -> {
@@ -59,7 +54,7 @@ public class SceneObserver implements IObserver {
             String name = (String) data[1];
             float[] labelColor = (float[]) data[2];
 
-            synchronized(view) {
+            synchronized (view) {
                 view.setEntity(entity);
                 view.setLabelColor(labelColor, name);
             }
