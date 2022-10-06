0fe4afea4dcb16360f020e6fb74f44d5fa18a45a
langurmonkey <me@tonisagrista.com>
2022-10-06 09:17:51 +0200

    none: bring over changes from master branch before switching.

--- core/src/gaiasky/util/comp/ViewAngleComparator.java
+++ core/src/gaiasky/util/comp/ViewAngleComparator.java
@@ -11,12 +11,12 @@ import gaiasky.scene.view.FocusView;
 import java.util.Comparator;
 
 /**
- * Compares entities depending on their solid angle.
+ * Compares entities depending on the solid angle from the camera.
  */
 public class ViewAngleComparator<T> implements Comparator<T> {
 
-    private FocusView view1 = new FocusView();
-    private FocusView view2 = new FocusView();
+    private final FocusView view1 = new FocusView();
+    private final FocusView view2 = new FocusView();
 
     @Override
     public int compare(T o1, T o2) {
