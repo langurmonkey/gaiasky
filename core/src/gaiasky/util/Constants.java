38ae7b632bf0b9aa627ef1399f9d914b8a694052
langurmonkey <me@tonisagrista.com>
2022-10-26 15:58:34 +0200

    none: change back-buffer scale slider step to 0.1.

--- core/src/gaiasky/util/Constants.java
+++ core/src/gaiasky/util/Constants.java
@@ -309,7 +309,7 @@ public class Constants {
     public static final float UI_SCALE_MAX = 3.0f;
     public static final float BACKBUFFER_SCALE_MIN = 0.5f;
     public static final float BACKBUFFER_SCALE_MAX = 4.0f;
-    public static final float BACKBUFFER_SCALE_STEP = 0.25f;
+    public static final float BACKBUFFER_SCALE_STEP = 0.1f;
 
     // Max time for VSOP87 algorithms
     public static final long MAX_VSOP_TIME_MS = 20000L * (long) Nature.Y_TO_MS;
