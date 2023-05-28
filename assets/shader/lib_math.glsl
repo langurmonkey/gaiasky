53698f9e5e2c9d063854df2d81d6b74a7dc8ada1
langurmonkey <me@tonisagrista.com>
2023-05-28 09:56:41 +0200

    perf: remove guard clauses in shader interpolation function that are already covered by `smoothstep()`.

--- assets/shader/lib_math.glsl
+++ assets/shader/lib_math.glsl
@@ -1,8 +1,6 @@
 #ifndef GLSL_LIB_MATH
 #define GLSL_LIB_MATH
 float lint(float x, float x0, float x1, float y0, float y1) {
-    if(x <= x0) return y0;
-    if(x >= x1) return y1;
     return y0 + (y1 - y0) * smoothstep(x0, x1, x);
 }
 float lint2(float x, float x0, float x1, float y0, float y1) {
