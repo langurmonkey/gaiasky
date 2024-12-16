48c17804e09c071296c91d0e861c4cf7dbc73eda
langurmonkey <me@tonisagrista.com>
2024-12-16 09:25:03 +0100

    none: change order of atmosphere and volumes.

--- core/src/gaiasky/render/RenderGroup.java
+++ core/src/gaiasky/render/RenderGroup.java
@@ -145,7 +145,7 @@ public enum RenderGroup {
     /**
      * Volume models.
      **/
-    VOLUME(5810),
+    VOLUME(6810),
     /**
      * Using normal shader for per-pixel lighting, rendered late for items with transparency.
      **/
