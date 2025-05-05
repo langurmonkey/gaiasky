7b5cd23aa9e3b68326aec08df1f101414c51225b
langurmonkey <me@tonisagrista.com>
2025-05-05 16:00:54 +0200

    none: fix billboard set initialization.

--- core/src/gaiasky/scene/system/initialize/BillboardSetInitializer.java
+++ core/src/gaiasky/scene/system/initialize/BillboardSetInitializer.java
@@ -11,13 +11,17 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import gaiasky.data.group.PointDataProvider;
 import gaiasky.scene.Mapper;
+import gaiasky.scene.api.IParticleRecord;
 import gaiasky.scene.record.BillboardDataset;
+import gaiasky.scene.record.ParticleVector;
 import gaiasky.scene.system.render.draw.text.LabelEntityRenderSystem;
 import gaiasky.scene.view.LabelView;
 import gaiasky.util.Constants;
 import gaiasky.util.Logger;
 import gaiasky.util.math.Vector3b;
 import gaiasky.util.math.Vector3d;
+
+import java.util.List;
 
 public class BillboardSetInitializer extends AbstractInitSystem {
 
@@ -45,36 +49,59 @@ public class BillboardSetInitializer extends AbstractInitSystem {
 
     @Override
     public void setUpEntity(Entity entity) {
+
+        transformData(entity);
     }
 
     public boolean reloadData(Entity entity) {
         try {
-            var body = Mapper.body.get(entity);
             var billboard = Mapper.billboardSet.get(entity);
-            var coord = Mapper.coordinates.get(entity);
-            var transform = Mapper.transform.get(entity);
-
-            // Set static coordinates to position
-            coord.coordinates.getEquatorialCartesianCoordinates(null, body.pos);
-
-            // Model
-            Vector3d aux = D31;
-            Vector3b pos3b = body.pos;
-
             var provider = new PointDataProvider();
             boolean reload = false;
             for (BillboardDataset dataset : billboard.datasets) {
-                boolean reloadNeeded = dataset.initialize(provider, (v) -> {
-                    aux.set(v);
-                    aux.scl(body.size).rotate(-90, 0, 1, 0).mul(transform.matrix).add(pos3b);
-                    v.set(aux);
-                }, reload);
+                boolean reloadNeeded = dataset.initialize(provider, null, reload);
                 reload = reload || reloadNeeded;
             }
             return reload;
         } catch (Exception e) {
-            Logger.getLogger(this.getClass()).error(e);
+            Logger.getLogger(this.getClass())
+                    .error(e);
         }
         return false;
+    }
+
+    public void transformData(Entity entity) {
+        var body = Mapper.body.get(entity);
+        var coord = Mapper.coordinates.get(entity);
+        var transform = Mapper.transform.get(entity);
+        var set = Mapper.billboardSet.get(entity);
+
+        // Set static coordinates to position
+        coord.coordinates.getEquatorialCartesianCoordinates(null, body.pos);
+
+        // Model
+        Vector3d aux = D31;
+        Vector3b pos3b = body.pos;
+
+        // Transform all
+        for (BillboardDataset bd : set.datasets) {
+            List<IParticleRecord> a = bd.data;
+            if (a != null) {
+                for (IParticleRecord iParticleRecord : a) {
+                    var pr = (ParticleVector) iParticleRecord;
+                    aux.set((float) pr.x(), (float) pr.z(), (float) pr.y());
+                    aux.scl(body.size)
+                            .rotate(-90, 0, 1, 0)
+                            .mul(transform.matrix)
+                            .add(pos3b);
+
+                    // We can modify the data vector because it is an array.
+                    var dat = pr.data();
+                    dat[0] = aux.x;
+                    dat[1] = aux.y;
+                    dat[2] = aux.z;
+                }
+            }
+        }
     }
 }
