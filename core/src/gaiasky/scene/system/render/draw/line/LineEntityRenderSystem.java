13382da4a2de9e9c6d68397d55b371c942ffa49e
langurmonkey <me@tonisagrista.com>
2025-06-19 08:46:14 +0200

    fix: Proper motions produce index out of bounds due to incorrect computation of maximum number of arrows.

--- core/src/gaiasky/scene/system/render/draw/line/LineEntityRenderSystem.java
+++ core/src/gaiasky/scene/system/render/draw/line/LineEntityRenderSystem.java
@@ -215,7 +215,7 @@ public class LineEntityRenderSystem {
                 previous = point;
             }
             // Join last to first.
-            Vector3D first = points.get(0);
+            Vector3D first = points.getFirst();
             renderer.addLine(lineView, (float) first.x, (float) first.y, (float) first.z, (float) previous.x, (float) previous.y, (float) previous.z, body.color[0], body.color[1], body.color[2], alpha * base.opacity);
         }
     }
@@ -540,7 +540,9 @@ public class LineEntityRenderSystem {
 
     private long getMaxProperMotionLines(Entity entity, StarSet set) {
         // Star sets in octrees get their number tuned down, because usually there's many of them in view.
-        return FastMath.min(set.pointData.size(), FastMath.max(Math.round(Settings.settings.scene.star.group.numVelocityVector / (Mapper.octant.has(entity) ? 5f : 1f)), Constants.MIN_VELOCITY_VECTORS_STAR_GROUP));
+        return FastMath.min(FastMath.min(set.pointData.size(), set.indices.length),
+                            FastMath.max(Math.round(Settings.settings.scene.star.group.numVelocityVector / (Mapper.octant.has(entity) ? 5f : 1f)),
+                                         Constants.MIN_VELOCITY_VECTORS_STAR_GROUP));
     }
 
     private int wrap(int idx, int n) {
