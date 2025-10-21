f52005fcf6d65bde97d23aff78ed0a8242ea517d
langurmonkey <me@tonisagrista.com>
2025-10-21 12:42:35 +0200

    none: Use additive blending (for now) to blend full- and half-res buffers.

--- assets/shader/postprocess/fullhalfresblend.frag.glsl
+++ assets/shader/postprocess/fullhalfresblend.frag.glsl
@@ -12,7 +12,7 @@ uniform sampler2D u_texture3;
 uniform vec2 u_zFarK;
 
 in vec2 v_texCoords;
-layout (location = 0) out vec4 fragColor;
+layout(location = 0) out vec4 fragColor;
 
 #include <shader/lib/logdepthbuff.glsl>
 
@@ -20,18 +20,23 @@ void main() {
     vec3 fullRes = texture(u_texture0, v_texCoords).rgb;
     vec3 halfRes = texture(u_texture1, v_texCoords).rgb;
 
-    // Recover 'linear' depth values.
-    float depthFull = 1.0 / recoverWValue(texture(u_texture2, v_texCoords).r, u_zFarK.x, u_zFarK.y);
-    float depthHalf = 1.0 / recoverWValue(texture(u_texture3, v_texCoords).r, u_zFarK.x, u_zFarK.y);
+    // Simple additive blend, for now.
+    fragColor = vec4(clamp(fullRes + halfRes, 0.0, 1.0), 1.0);
 
-    if (depthFull < depthHalf) {
-        // Full-res pixel is closer - use it
-        fragColor = vec4(fullRes, 1.0);
-    } else if (depthFull > depthHalf) {
-        // Half-res pixel is closer - use it
-        fragColor = vec4(halfRes, 1.0);
-    } else {
-        // Depths are approximately equal - blend
-        fragColor = vec4(clamp(fullRes + halfRes, 0.0, 1.0), 1.0);
-    }
+    // Blend using depth buffers.
+    //
+    // // Recover 'linear' depth values.
+    // float depthFull = 1.0 / recoverWValue(texture(u_texture2, v_texCoords).r, u_zFarK.x, u_zFarK.y);
+    // float depthHalf = 1.0 / recoverWValue(texture(u_texture3, v_texCoords).r, u_zFarK.x, u_zFarK.y);
+
+    // if (depthFull < depthHalf) {
+    //     // Full-res pixel is closer - use it
+    //     fragColor = vec4(fullRes, 1.0);
+    // } else if (depthFull > depthHalf) {
+    //     // Half-res pixel is closer - use it
+    //     fragColor = vec4(halfRes, 1.0);
+    // } else {
+    //     // Depths are approximately equal - blend
+    //     fragColor = vec4(clamp(fullRes + halfRes, 0.0, 1.0), 1.0);
+    // }
 }
