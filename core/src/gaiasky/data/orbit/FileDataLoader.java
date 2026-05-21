langurmonkey/gaiasky

commit f4601ac3aee0d66812bc5a78abe8b5d442576446

feat: Improve treatment of spacecrafts and missions that include dataset cards. Missions now can be 'selected', which means that the time is set to the start of the mission, and the spacecraft is set as the focus. Part of #908.

core/src/gaiasky/data/orbit/FileDataLoader.java
@@ -16,8 +16,12 @@ import gaiasky.util.parse.Parser;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 
 /**
  * Loads orbit data from an ASCII text file.
@@ -55,20 +59,34 @@ public class FileDataLoader {
         super();
     }
 
+    private static final DateTimeFormatter FORMAT = new DateTimeFormatterBuilder()
+            .appendPattern("yyyy-MM-dd_HH:mm:ss")
+            // Make the .SSS part optional
+            .optionalStart()
+            .appendFraction(ChronoField.MILLI_OF_SECOND, 0, 3, true)
+            .optionalEnd()
+            .toFormatter();
+
     /**
-     * Parses the given time and returns the number of milliseconds from the epoch of 1970-01-01T00:00:00Z.
-     *
-     * @param token The token to parse.
-     * @return time as milliseconds from the epoch of 1970-01-01T00:00:00Z
+     * Parse a time string with the format {@link #FORMAT} in UTC.
+     * @param token The string.
+     * @return The time as the number of seconds to epoch (see {@link Instant}).
      */
     public long parseTime(String token) {
         try {
-            Timestamp t = Timestamp.valueOf(token.replace('_', ' '));
-            return t.getTime();
+            // Handles both "2023-07-07_13:29:42.000"
+            // and "2023-07-07_13:29:42"
+            LocalDateTime ldt = LocalDateTime.parse(token, FORMAT);
+            return ldt.toInstant(ZoneOffset.UTC).toEpochMilli();
+
         } catch (Exception e) {
-            // Not a timestamp. Possibly a julian date.
-            double jd = Parser.parseDouble(token);
-            return AstroUtils.julianDateToInstant(jd).toEpochMilli();
+            // Fallback for Julian dates remains the same.
+            try {
+                double jd = Double.parseDouble(token);
+                return AstroUtils.julianDateToInstant(jd).toEpochMilli();
+            } catch (Exception nfe) {
+                throw new IllegalArgumentException("Unable to parse token: " + token);
+            }
         }
     }
 

assets/i18n/gsbundle.properties
@@ -2039,6 +2039,8 @@ gui.tooltip.controller.blacklist=Blacklisted in configuration file.
 gui.tooltip.data.highaccuracy=Results in more accurate positions for planets and moons, but performance will decrease, specially in old CPUs.
 # Toggle visibility of dataset
 gui.tooltip.dataset.toggle=Toggle visibility of dataset.
+# Go to mission start
+gui.tooltip.dataset.mission.goto=Go to mission spacecraft and set time to mission start, if applicable.
 # Remove dataset
 gui.tooltip.dataset.remove=Remove dataset.
 # Highlight dataset

assets/shader/lib/lines.glsl
@@ -3,7 +3,7 @@
 
 // Determine how thick you want the "solid" part of the line (0.0 to 1.0).
 // 0.6 means the line is solid for 60% of the quad width and fades at the last 40%
-#define LINE_THICKNESS 0.4
+#define LINE_THICKNESS 0.5
 // Use new method based on screen-space rate of change.
 // Comment out to use the legacy method based on a power function.
 #define LINE_FWIDTH

core/src/gaiasky/GaiaSky.java
@@ -1485,7 +1485,7 @@ public final class GaiaSky implements ApplicationListener, IObserver {
         return this.sceneRenderer.alpha(cts);
     }
 
-    public Optional<CatalogInfo> getCatalogInfoFromEntity(Entity entity) {
+    public Optional<DatasetCard> getCatalogInfoFromEntity(Entity entity) {
         if (Mapper.datasetDescription.has(entity)) {
             return gaiaSkyAssets.catalogManager.getByEntity(entity);
         }

core/src/gaiasky/data/OctreeLoader.java
@@ -28,8 +28,8 @@ import gaiasky.scene.system.initialize.BaseInitializer;
 import gaiasky.scene.system.initialize.ParticleSetInitializer;
 import gaiasky.scene.system.initialize.SceneGraphBuilderSystem;
 import gaiasky.scene.view.OctreeObjectView;
-import gaiasky.util.CatalogInfo;
-import gaiasky.util.CatalogInfo.CatalogInfoSource;
+import gaiasky.util.DatasetCard;
+import gaiasky.util.DatasetCard.DatasetSourceType;
 import gaiasky.util.Logger;
 import gaiasky.util.Logger.Log;
 import gaiasky.util.Settings;
@@ -219,7 +219,7 @@ public class OctreeLoader extends AbstractSceneLoader implements IObserver, IOct
             // Catalog info
             String name = this.name != null ? this.name : "LOD data";
             String description = this.description != null ? this.description : "Octree-based LOD dataset";
-            CatalogInfo ci = new CatalogInfo(name, description, null, CatalogInfoSource.LOD, 1.5f, entity);
+            DatasetCard ci = new DatasetCard(name, description, null, DatasetSourceType.LOD, 1.5f, entity);
             ci.nParticles = params.containsKey("nObjects") ? (Long) params.get("nObjects") : -1;
             ci.nParticles = ci.nParticles < 0 && params.containsKey("nobjects") ? (Long) params.get("nobjects") : -1;
             ci.sizeBytes = params.containsKey("size") ? (Long) params.get("size") : -1;
@@ -554,7 +554,7 @@ public class OctreeLoader extends AbstractSceneLoader implements IObserver, IOct
         set.setEpoch(epoch);
 
         var sgDatasetDesc = Mapper.datasetDescription.get(sg);
-        sgDatasetDesc.setCatalogInfoBare(datasetDesc.catalogInfo);
+        sgDatasetDesc.setCatalogInfoBare(datasetDesc.datasetCard);
 
         var sgOctant = Mapper.octant.get(sg);
 

core/src/gaiasky/gui/components/DatasetsComponent.java
@@ -18,6 +18,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Timer;
 import gaiasky.GaiaSky;
 import gaiasky.event.Event;
 import gaiasky.event.EventManager;
@@ -30,11 +31,15 @@ import gaiasky.gui.window.ColorPicker;
 import gaiasky.gui.window.ColorPickerAbstract;
 import gaiasky.gui.window.ColormapPicker;
 import gaiasky.scene.Mapper;
+import gaiasky.scene.camera.CameraManager;
+import gaiasky.scene.entity.EntityUtils;
 import gaiasky.scene.view.FocusView;
 import gaiasky.util.*;
+import gaiasky.util.coord.IOrbitCoordinates;
 import gaiasky.util.i18n.I18n;
 import gaiasky.util.scene2d.*;
 
+import java.time.Instant;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -78,9 +83,9 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
         group = new VerticalGroup();
         group.columnAlign(Align.left);
 
-        Collection<CatalogInfo> cis = this.catalogManager.getCatalogInfos();
+        Collection<DatasetCard> cis = this.catalogManager.getCatalogInfos();
         if (cis != null) {
-            for (CatalogInfo ci : cis) {
+            for (DatasetCard ci : cis) {
                 addCatalogInfo(ci);
             }
         }
@@ -90,7 +95,70 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
         addNoDatasets();
     }
 
-    private void setDatasetVisibility(CatalogInfo ci,
+    private void goToMissionStart(DatasetCard ci,
+                                  OwnImageButton button,
+                                  Actor source) {
+        if (ci.entity != null && ci.isMission()) {
+            var catalogEntity = ci.entity;
+            var satellite = EntityUtils.getFirstChildWithComponent(catalogEntity, Mapper.parentOrientation);
+            if (satellite != null) {
+                // Let's see if we have a start time.
+                Instant start = null;
+                var coordinates = Mapper.coordinates.get(satellite);
+                if (coordinates.coordinates instanceof IOrbitCoordinates oc) {
+                    var orbit = oc.getOrbitObject();
+                    if (orbit != null) {
+                        // Our orbit is here.
+                        var tr = Mapper.trajectory.get(orbit);
+                        if (!tr.closedLoop) {
+                            var verts = Mapper.verts.get(orbit);
+                            if (verts != null && verts.pointCloudData != null && !verts.pointCloudData.samples.isEmpty()) {
+                                start = verts.pointCloudData.getStart();
+                            }
+                        }
+                    }
+                }
+                if (start == null) {
+                    // We do not have time, only go to object.
+                    GaiaSky.postRunnable(() -> {
+                        // First, set camera mode and focus.
+                        EventManager.publish(Event.CAMERA_MODE_CMD, source, CameraManager.CameraMode.FOCUS_MODE, true);
+                        EventManager.publish(Event.FOCUS_CHANGE_CMD, source, satellite, true);
+                        GaiaSky.postRunnable(() -> {
+                            // Go to object.
+                            EventManager.publish(Event.GO_TO_OBJECT_CMD, this);
+                        });
+                    });
+                } else {
+                    final var time = start;
+                    GaiaSky.postRunnable(() -> {
+                        // First, time.
+                        EventManager.publish(Event.TIME_CHANGE_CMD, this, time);
+
+                        // Second, focus after a delay.
+                        var task = new Timer.Task() {
+                            @Override
+                            public void run() {
+                                // Set camera mode and focus.
+                                EventManager.publish(Event.CAMERA_MODE_CMD, source, CameraManager.CameraMode.FOCUS_MODE, true);
+                                EventManager.publish(Event.FOCUS_CHANGE_CMD, source, satellite, true);
+                                GaiaSky.postRunnable(() -> {
+                                    // Go to object.
+                                    EventManager.publish(Event.GO_TO_OBJECT_CMD, this);
+                                });
+
+                            }
+                        };
+                        Timer.schedule(task, 0.5f);
+                    });
+                }
+            }
+        }
+
+    }
+
+
+    private void setDatasetVisibility(DatasetCard ci,
                                       OwnImageButton eye,
                                       boolean visible,
                                       Actor source) {
@@ -106,7 +174,7 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
         }
     }
 
-    private void setDatasetHighlight(CatalogInfo ci,
+    private void setDatasetHighlight(DatasetCard ci,
                                      OwnImageButton mark,
                                      boolean highlight,
                                      Actor source) {
@@ -118,7 +186,7 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
         }
     }
 
-    private void showDatasetFilters(CatalogInfo ci) {
+    private void showDatasetFilters(DatasetCard ci) {
         if (!filtersMap.containsKey(ci.name)) {
             var dfw = new DatasetFiltersWindow(ci, skin, stage);
             dfw.setVisible(true);
@@ -131,7 +199,7 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
         }
     }
 
-    private void showDatasetInfo(CatalogInfo ci) {
+    private void showDatasetInfo(DatasetCard ci) {
         if (!infoMap.containsKey(ci.name)) {
             var diw = new DatasetInfoWindow(ci, skin, stage);
             diw.setVisible(true);
@@ -144,7 +212,7 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
         }
     }
 
-    private void showDatasetVisualSettings(CatalogInfo ci) {
+    private void showDatasetVisualSettings(DatasetCard ci) {
         if (!visualSettingsMap.containsKey(ci.name)) {
             var dpw = new DatasetVisualSettingsWindow(ci, skin, stage);
             dpw.setVisible(true);
@@ -157,7 +225,7 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
         }
     }
 
-    private void showDatasetTransforms(CatalogInfo ci) {
+    private void showDatasetTransforms(DatasetCard ci) {
         if (!transformsMap.containsKey(ci.name)) {
             var dtw = new DatasetTransformsWindow(ci, skin, stage);
             dtw.setVisible(true);
@@ -170,9 +238,10 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
         }
     }
 
-    private void addCatalogInfo(CatalogInfo ci) {
+    private void addCatalogInfo(DatasetCard ci) {
         // Controls
         Table controls = new Table(skin);
+
         OwnImageButton visibilityButton = new OwnImageButton(skin, "eye-toggle");
         visibilityButton.setCheckedNoFire(!ci.isVisible(true));
         visibilityButton.addListener(new OwnTextTooltip(I18n.msg("gui.tooltip.dataset.toggle"), skin));
@@ -194,6 +263,18 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
             }
             return false;
         });
+
+        OwnImageButton goToButton = new OwnImageButton(skin, "go-to");
+        goToButton.setCheckedNoFire(!ci.isVisible(true));
+        goToButton.addListener(new OwnTextTooltip(I18n.msg("gui.tooltip.dataset.mission.goto"), skin));
+        goToButton.addListener(event -> {
+            if (event instanceof ChangeEvent) {
+                goToMissionStart(ci, goToButton, goToButton);
+                return true;
+            }
+            return false;
+        });
+
 
         OwnImageButton filtersButton = new OwnImageButton(skin, "filter");
         filtersButton.addListener(new OwnTextTooltip(I18n.msg("gui.tooltip.dataset.filter"), skin));
@@ -257,7 +338,15 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
         } else {
             controls.add(visibilityButton).padRight(pad30);
         }
-        controls.add(transformsButton).padRight(pad30);
+        // Can only add arbitrary transformations to star and particle sets (and octrees).
+        if (ci.hasParticleAttributes()) {
+            controls.add(transformsButton).padRight(pad30);
+        }
+        // Go to object for mission datasets.
+        if (ci.isMission()) {
+            controls.add(goToButton).padRight(pad30);
+        }
+
         controls.add(infoButton).padRight(pad6);
         controls.add(rubbishButton);
 
@@ -318,11 +407,11 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
 
         if (ci.isHighlightable()) {
             var sizeScaling = new OwnSliderReset(I18n.msg("gui.dataset.size"),
-                                                          Constants.MIN_POINT_SIZE_SCALE,
-                                                          Constants.MAX_POINT_SIZE_SCALE,
-                                                          Constants.SLIDER_STEP_TINY,
-                                                          1f,
-                                                          skin);
+                                                 Constants.MIN_POINT_SIZE_SCALE,
+                                                 Constants.MAX_POINT_SIZE_SCALE,
+                                                 Constants.SLIDER_STEP_TINY,
+                                                 1f,
+                                                 skin);
             sizeScaling.setWidth(350f);
             if (ci.entity != null) {
                 var graph = Mapper.graph.get(ci.entity);
@@ -475,7 +564,7 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
         switch (event) {
             case CATALOG_ADD -> {
                 removeNoDatasets();
-                addCatalogInfo((CatalogInfo) data[0]);
+                addCatalogInfo((DatasetCard) data[0]);
             }
             case CATALOG_REMOVE -> {
                 String datasetName = (String) data[0];
@@ -516,7 +605,7 @@ public class DatasetsComponent extends GuiComponent implements IObserver {
             }
             case CATALOG_HIGHLIGHT -> {
                 if (source != this) {
-                    CatalogInfo ci = (CatalogInfo) data[0];
+                    DatasetCard ci = (DatasetCard) data[0];
                     float[] col = ci.hlColor;
                     if (colorMap.containsKey(ci.name) && col != null) {
                         if (ci.plainColor) {

core/src/gaiasky/gui/datasets/DatasetFiltersWindow.java
@@ -19,7 +19,7 @@ import gaiasky.gui.window.GenericDialog;
 import gaiasky.scene.Mapper;
 import gaiasky.scene.api.IParticleRecord;
 import gaiasky.scene.record.ParticleType;
-import gaiasky.util.CatalogInfo;
+import gaiasky.util.DatasetCard;
 import gaiasky.util.Logger;
 import gaiasky.util.TextUtils;
 import gaiasky.util.filter.Filter;
@@ -39,13 +39,13 @@ import java.util.Locale;
 public class DatasetFiltersWindow extends GenericDialog {
     private static final Logger.Log logger = Logger.getLogger(DatasetFiltersWindow.class);
 
-    private final CatalogInfo ci;
+    private final DatasetCard ci;
     private Table filterTable;
     private Filter filter;
     private OwnTextIconButton addFilterOrRule;
     private boolean filterEdited;
 
-    public DatasetFiltersWindow(CatalogInfo ci,
+    public DatasetFiltersWindow(DatasetCard ci,
                                 Skin skin,
                                 Stage stage) {
         super(I18n.msg("gui.dataset.filters") + " - " + ci.name, skin, stage);

core/src/gaiasky/gui/datasets/DatasetInfoWindow.java
@@ -11,7 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import gaiasky.GaiaSky;
 import gaiasky.gui.window.GenericDialog;
-import gaiasky.util.CatalogInfo;
+import gaiasky.util.DatasetCard;
 import gaiasky.util.GlobalResources;
 import gaiasky.util.i18n.I18n;
 import gaiasky.util.scene2d.OwnLabel;
@@ -22,9 +22,9 @@ import gaiasky.util.scene2d.OwnTextArea;
  * A window that displays information on a particular dataset.
  */
 public class DatasetInfoWindow extends GenericDialog {
-    private final CatalogInfo ci;
+    private final DatasetCard ci;
 
-    public DatasetInfoWindow(CatalogInfo ci,
+    public DatasetInfoWindow(DatasetCard ci,
                              Skin skin,
                              Stage stage) {
         super(I18n.msg("gui.dataset.info") + " - " + ci.name, skin, stage);
@@ -66,7 +66,7 @@ public class DatasetInfoWindow extends GenericDialog {
 
     private OwnScrollPane getOwnScrollPane() {
         String descriptionString = ci.description != null ? ci.description : ci.name;
-        OwnTextArea descriptionTextArea = new OwnTextArea(descriptionString, skin, "no-disabled");
+        OwnTextArea descriptionTextArea = new OwnTextArea(descriptionString, skin, "info");
         descriptionTextArea.setWidth(600f);
         descriptionTextArea.setDisabled(true);
         float fontHeight = descriptionTextArea.getStyle().font.getLineHeight();

commit 0fb6f30af34d3b99d50e514e5de5a703801075d4

none: Fix regression introduced in f4601ac3aee0d in JD parsing. Unrelated part of #905.

core/src/gaiasky/data/orbit/FileDataLoader.java
@@ -82,7 +82,7 @@ public class FileDataLoader {
         } catch (Exception e) {
             // Fallback for Julian dates remains the same.
             try {
-                double jd = Double.parseDouble(token);
+                double jd = Parser.parseDouble(token);
                 return AstroUtils.julianDateToInstant(jd).toEpochMilli();
             } catch (Exception nfe) {
                 throw new IllegalArgumentException("Unable to parse token: " + token);