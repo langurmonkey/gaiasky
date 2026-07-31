739dc9535dd19772aeaf911c09c3aae6f98b020f
langurmonkey <me@tonisagrista.com>
2026-07-31 09:15:27 +0200

    none: Trying out different blending modes for the atmosphere.

--- core/src/gaiasky/scene/record/AtmosphereComponent.java
+++ core/src/gaiasky/scene/record/AtmosphereComponent.java
@@ -93,6 +93,7 @@ public final class AtmosphereComponent extends NamedComponent implements IUpdata
 
             setUpAtmosphericScatteringMaterial(atmMat);
             atmMat.set(new BlendingAttribute(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA));
+            //atmMat.set(new BlendingAttribute(GL20.GL_ONE, GL20.GL_ONE));
             // CREATE ATMOSPHERE MODEL
             mc.instance = new IntModelInstance(atmosphereModel, this.localTransform);
         } else {
